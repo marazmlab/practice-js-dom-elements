9d9246e07939c1b190904e22a70e6c12dfdd037d
Marek Bełz <belzmarek@gmail.com>
2024-12-21 10:14:35 +0000

    done

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,11 @@
-console.log('DOM');
+
+const liElement = document.querySelector('.comments__item.comments__item--newest');
+const dataInfoList = liElement.querySelectorAll('[data-info]');
+
+if(liElement && dataInfoList) {
+    console.log(`Znaleziono ${dataInfoList.length} poniższe elementy z atrybutem  data-info :`);
+
+    dataInfoList.forEach(function(element) {
+        console.log(element)
+    });
+}
