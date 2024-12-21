0b207a30e076adc348653f3ee5a24a66109eafc1
Marek Bełz <belzmarek@gmail.com>
2024-12-21 14:07:54 +0000

    done

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,45 @@
-console.log('DOM');
-
 const curr = document.querySelector('.js-curr');
+
+// 
+const newButton = document.createElement('button');
+newButton.classList.add('article__btn');
+newButton.textContent = 'usuń z koszyka'
+
+curr.parentElement.insertBefore(newButton, curr.nextElementSibling);
+
+
+// 
+let sibling = curr.parentElement.firstElementChild;
+while (sibling) {
+  if (sibling !== curr) {
+    sibling.classList.add('siblings');
+  }
+  sibling = sibling.nextElementSibling;
+}
+
+
+// 
+const nextArticle = curr.closest('.article').nextElementSibling;
+if(nextArticle && nextArticle.classList.contains('article')) {
+    nextArticle.setAttribute('title', 'nextElementSibling');
+}
+
+
+// 
+const lastArticle = document.querySelector('.articles__item:last-child');
+const paragraph = document.createElement('p');
+paragraph.textContent = 'Dodskowy paragraf.';
+const button = lastArticle.querySelector('.article__btn');
+lastArticle.insertBefore(paragraph, button);
+
+// 
+const newArticle = document.createElement('article');
+newArticle.classList.add('articles__item', 'article');
+const articlesSection = document.querySelector('.articles');
+articlesSection.insertBefore(newArticle, articlesSection.firstElementChild);
+
+newArticle.innerHTML = `
+  <h1 class="article__title">JS- Nowy Artykuł</h1>
+  <p class="article__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quo quibusdam, nemo neque consequuntur pariatur totam? Facere quaerat molestias hic.</p>
+  <button class="article__btn">Kupuję!</button>
+`;
