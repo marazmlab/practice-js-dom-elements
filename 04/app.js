639f69bb85da065165c43d61177b9a974b9388c7
Marek Bełz <belzmarek@gmail.com>
2024-12-21 12:20:31 +0000

    done

--- 04/app.js
+++ 04/app.js
@@ -1,4 +1,3 @@
-console.log('DOM');
 
 // struktura do wykorzystania w pętli
 const menuItems = [
@@ -6,3 +5,20 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const nav = document.querySelector('nav');
+const ul = document.createElement('ul');
+
+
+
+menuItems.forEach(function(item) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = item.url;    
+    a.textContent = item.text;
+
+    li.appendChild(a);
+    ul.appendChild(li);
+});
+
+nav.appendChild(ul);
